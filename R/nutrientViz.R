9e31cae159bb3da824422ee60e61eec2e959be1d
coffm049 <coffm049@umn.edu>
2023-06-01 20:29:53 -0500

    Developed nutrient vizualization function
    Fixes #2

--- R/nutrientViz.R
+++ R/nutrientViz.R
@@ -5,7 +5,6 @@
 #' parameter combination. Requires columns, Type, Date, Site, Round, Year in any order
 #' @param data 
 #' @examples cleanNuts(data)
-#' @export
 #' @import dplyr, tidyr
 #' @importFrom magrittr %>%
 #' @return tall clean dataframe for vizualization 
@@ -23,22 +22,25 @@ cleanNuts <- function(data) {
                             "Chl.A..ug.L."="Chlorphyll A (µg/L)", "Nitrate.nitrite..mg.L."="Nitrate + nitrite (mg/L)",
                             "Ortho.P..mg.L."="Ortho-P (mg/L)","TN..mg.L."="TN (mg/L)", "TP..mg.L."="TP (mg/L)", "TSS..mg.L."="TSS (mg/L)" )) %>%
     dplyr::group_by(Date, Site, Parameter) %>%
-    dplyr::summarize(value=mean(value, na.rm = T))
+    dplyr::summarize(value=mean(value, na.rm = T)) %>% 
+    # drop unobserved factors in Site
+    dplyr::mutate(Site = factor(Site))
 }
 
 
 #' nutsThroughTime
 #'
 #' Creates a ggplot object of nutrient data through time. Specifically, it creates a ggplot object 
-#' @param data a tall clean dataframe with columns, Site, value, Parameter
+#' @param dataframe with site, date, and parameters in other columns 
 #' @examples nutsThroughTime(data)
 #' @export
+#' @example nutsThroughTime(df)
 #' @import dplyr
 #' @import ggplot2
 #' @importFrom magrittr %>%
 #' @return ggplot object
 nutsThroughTime <- function(data) {
-  data %>%
+  cleanNuts(data) %>%
       dplyr::mutate(day1=ifelse(as.numeric(Site)<8,"West","East"),
              day1 = factor(day1, levels = c("West", "East"))) %>%
     ggplot2::ggplot(aes(x=Date, y=value))+
@@ -47,19 +49,15 @@ nutsThroughTime <- function(data) {
     ggplot2::facet_grid(row= vars(Parameter), cols=vars(Site), scales = "free_y",labeller = label_wrap_gen(width=10)) +
     ggplot2::labs(title = "Nutrient Data", x='2022 Dates', y = 'Parameter Value', subtitle = 'Site')+
     ggplot2::theme(
-      plot.title = element_text(hjust=0.5, size = 20, margin = margin(0.5,0,0.5,0, 'cm')),
+      plot.title = element_text(hjust=0.5, size = 20, margin = margin(0,0,0,0, 'cm')),
       plot.subtitle = element_text(hjust=0.5, size=20),
-      axis.text.x = element_text(size = 16, angle=45, hjust=1),
-      axis.text.y = element_text(size = 16),
+      axis.text.x = element_text(angle=45, hjust=1),
+      axis.title.y = element_blank(),
       axis.line = element_line(size=1),
-      axis.title = element_text(size=20),
-      strip.text.x = element_text(size=16),
-      strip.text.y = element_text(size=10, margin = margin(1,1,1,1, 'cm')),
+      strip.text.y = element_text(angle=0),
       panel.grid.major = element_line(size=1),
       panel.border = element_rect(size=1, fill=NA)
-    ) +
-    ggplot2::theme(strip.text.y = element_text(angle = 0, vjust = 0, hjust=0))
-
+    )
 }
 
 
