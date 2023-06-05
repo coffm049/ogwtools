6bf25c42e95c3df0c2ee715a59cd5e3c157c8e44
coffm049 <coffm049@umn.edu>
2023-06-05 13:12:13 -0500

    Edited watercolumn vizualization

--- R/waterColumnViz.R
+++ R/waterColumnViz.R
@@ -2,19 +2,21 @@
 #'
 #' Vizualize water quality variables along the water column for multiple time
 #' points and multiple site
-#' @param df dataframe with sites, rounds, depth, and water quality variables
-#' of interes in long format
+#' @param df dataframe with round, date, site, depth, followed by parameter columns
 #' @param variable string specifyning which variable to vizualize
 #' @examples myfunction(X,Y)
-#' @import ggplot2
+#' @import ggplot2, dplyr, tidyr
 #' @export
 #' @return ggplot plot
-waterColumnViz <- function(df, variable) {
-  # ggplot where the x aesthetic is the variable of interest
-  ggplot2::ggplot(data = df, aes(x = get(variable), y = depth, col = factor(round), group= round)) +
+waterColumnViz <- function(df, parameter, parameter_name_plot) {
+  df %>%
+    dplyr::na_if("") %>%
+    mutate(Date = as.Date(Date, format = "%m/%d/%Y")) %>%
+    ggplot2::ggplot(aes_string(x = parameter, y = "Depth..m.", col = "Date", group= "Date")) +
     ggplot2::geom_path() +
-    ggplot2::facet_wrap(~site) +
-    ggplot2::xlab("") +
+    ggplot2::facet_wrap(~Site) +
     # set legend title
-    ggplot2::labs(col = "Round")
+    ggplot2::labs(col = "Date") + 
+  ylab("Depth (m)") + 
+  xlab(parameter_name_plot)
 }
